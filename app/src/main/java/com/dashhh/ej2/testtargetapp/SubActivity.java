1460db2c1aab3c842b55918fec998fbdb26c7036
ejDashh <devil1032@gmail.com>
2018-10-19 19:20:18 +0900

    !

--- app/src/main/java/com/dashhh/ej2/testtargetapp/SubActivity.java
+++ app/src/main/java/com/dashhh/ej2/testtargetapp/SubActivity.java
@@ -1,7 +1,7 @@
 package com.dashhh.ej2.testtargetapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
 import com.dashhh.ej2.testtargetapp.ChatRoomAdapter_Presenter.Adapter;
@@ -10,6 +10,11 @@ import com.dashhh.ej2.testtargetapp.ChatRoomAdapter_Presenter.Set;
 import java.util.ArrayList;
 
 public class SubActivity extends AppCompatActivity {
+    String ownerName;
+    String repoName;
+    String repoDescription;
+    String repoStarCount;
+    String ownerAvatarImage;
     private Adapter recyclerAdapter;
     private ArrayList<Set> recyclerSet = new ArrayList<>();
     private RecyclerView recyclerView;
@@ -20,21 +25,21 @@ public class SubActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sub);
 
 
-
     }
-
 
 
     public void adap() {
 
 
         Set setData = new Set(
-
-
+                ownerName,
+                repoName,
+                repoDescription,
+                repoStarCount,
+                ownerAvatarImage
         );
         recyclerSet.add(setData);
         recyclerAdapter.notifyDataSetChanged();
-        recyclerView.scrollToPosition(recyclerSet.size() - 1);
 
 
     }
